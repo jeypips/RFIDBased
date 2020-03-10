775bb8504b843b8c82e2034c53029a2b73ccbc4d
Jp <johnpaulbalanon6@gmail.com>
2020-03-10 16:56:46 +0800

    ..

--- handlers/daily/data.php
+++ handlers/daily/data.php
@@ -19,12 +19,28 @@ for ($i = 1; $i < 10; $i++) {
 	);
 }
 
+$footer = [];
+for ($i = 8; $i < 13; $i++) {
+	$footer[] = array(
+		"t"=>"$i:00"
+	);
+}
+
+for ($i = 1; $i < 10; $i++) {
+	$footer[] = array(
+		"t"=>"$i:00"
+	);
+}
+
 $_courses = $con->getData("SELECT * FROM course");
 
 $courses = [];
+$all = [];
+
 foreach ($_courses as $ci => $course) {
 
 	$content = [];
+	$total = [];
 	
 	$content[] = array(
 		"d"=>$course['cour_description']
@@ -51,42 +67,68 @@ foreach ($_courses as $ci => $course) {
 		$end_h = date("Y-m-d H:i:s",strtotime("+1 Hour",strtotime($start_h)));
 
 		$logs = $con->getData("SELECT * FROM logged_book lb INNER JOIN students s ON s.stud_id = lb.stud_id INNER JOIN course c ON c.cour_id = s.f_cour_id WHERE s.f_cour_id = '$cour' AND logb_login >= '$start_h' AND logb_login < '$end_h'");
-		/* $logs = $con->getData("SELECT COUNT(*) as countPerHour
+		
+		$content[] = array(
+			"d"=>count($logs)
+		);
+		
+		/* $t = $con->getData("SELECT COUNT(*) as countPerHour
 							FROM logged_book lb
 							INNER JOIN students s
 							ON s.stud_id = lb.stud_id
 							INNER JOIN course c
 							ON c.cour_id = s.f_cour_id
-							WHERE lb.logb_login >= '$start_h' AND logb_login < '$end_h'"); */
-							
-
-		$content[] = array(
-			"d"=>count($logs)
-		);
+							WHERE lb.logb_login >= '$start_h' AND logb_login < '$end_h'");
+		$total[] = array(
+			"d"=>$t
+		); */
+	
 		
 	};
 	
+	foreach ($footer as $hi => $h) {
+		
+		$h = $h['t'];
+		
+		if ($hi>4) {
+			
+			$ex_h = explode(":",$h);
+			$h_no = intval($ex_h[0])+12;
+			
+			$h = $h_no.":".$ex_h[1];
+			
+		};
+		
+		$cour = $course['cour_id'];
+		
+		$h = str_pad($h,5,"0",STR_PAD_LEFT);
+		
+		$start_h = date("Y-m-d ").$h.":00";
+		$end_h = date("Y-m-d H:i:s",strtotime("+1 Hour",strtotime($start_h)));
+
+		$logz = $con->getData("SELECT COUNT(*) as countPerHour
+							FROM logged_book lb
+							INNER JOIN students s
+							ON s.stud_id = lb.stud_id
+							INNER JOIN course c
+							ON c.cour_id = s.f_cour_id
+							WHERE lb.logb_login BETWEEN '$start_h' AND '$end_h'");
+		
+		$total[] = array(
+			"d"=>$logz
+		);
+	};
+	
 	$courses[] = $content;
+	$all[] = $content;
+	
 
 };
-
-$footer = [];
-for ($i = 8; $i < 13; $i++) {
-	$footer[] = array(
-		"t"=>0
-	);
-}
-
-for ($i = 1; $i < 10; $i++) {
-	$footer[] = array(
-		"t"=>0
-	);
-}
 
 $daily = array(
 	"header"=>$header,
 	"courses"=>$courses,
-	"footer"=>$footer
+	"footer"=>$all
 );
 
 header("Content-Type: application/json");
