5328d982abe7e3a107e7453bb0c33718d20f7274
Jp <johnpaulbalanon6@gmail.com>
2020-03-25 21:04:23 +0700

    ..

---
+++ handlers/reports/printStudents.php
@@ -0,0 +1,32 @@
+<?php
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+require_once '../../db.php';
+
+$con = new pdo_db();
+
+$dateNow =date("Y-m-d");
+
+$datas = $con->getData("SELECT *, DATE_FORMAT(logb_login, '%M %d, %Y') logb_login FROM logged_book WHERE date(logb_login) = '$dateNow'");
+
+foreach($datas as $key => $s){
+	
+	$students = $con->getData("SELECT *, CONCAT(stud_fName,' ',stud_lName) fullname FROM students WHERE students.stud_id = ".$s['stud_id']);
+		foreach($students as $i => $d){
+		
+			$section = $con->getData("SELECT * FROM section WHERE sect_id = ".$d['stud_sect_id']);
+			$students[$i]['stud_sect_id'] = $section[0];
+			
+			$year = $con->getData("SELECT * FROM year WHERE year_id = ".$d['stud_year_id']);
+			$students[$i]['stud_year_id'] = $year[0];
+			
+		};
+	$datas[$key]['stud_id'] = $students[0];
+	
+};
+
+header("Content-Type: application/json");
+echo json_encode($datas);
+
+?>
---
+++ handlers/reports/students.php
@@ -0,0 +1,64 @@
+<?php
+
+$_POST = json_decode(file_get_contents('php://input'), true);
+
+require_once '../../db.php';
+
+$con = new pdo_db();
+
+$datePick = $_POST['pickDate'];
+
+$newDate = date("Y-m-d ", strtotime($datePick));
+
+// $students = $con->getData("SELECT *, DATE_FORMAT(logb_login, '%M %d, %Y') logb_login FROM logged_book");
+
+$cour_id = isset($_POST['course']['cour_id'])?$_POST['course']['cour_id']:"";
+$year_id = isset($_POST['year']['year_id'])?$_POST['year']['year_id']:"";
+
+if($cour_id==""&&$year_id==!"") {
+	
+	$students = $con->getData("SELECT *, CONCAT(stud_fName,' ',stud_lName) fullname FROM students WHERE stud_year_id = '$year_id'");
+
+} else if ($cour_id==!""&&$year_id=="")  {
+	
+	$students = $con->getData("SELECT *, CONCAT(stud_fName,' ',stud_lName) fullname FROM students WHERE f_cour_id = '$cour_id'");
+
+} else if ($cour_id==!""&&$year_id==!""){
+	
+	$students = $con->getData("SELECT *, CONCAT(stud_fName,' ',stud_lName) fullname FROM students WHERE f_cour_id = '$cour_id' AND stud_year_id = '$year_id'");
+
+} else {
+	
+	$students = $con->getData("SELECT *, CONCAT(stud_fName,' ',stud_lName) fullname FROM students");
+	
+};
+
+foreach($students as $key => $s){
+	
+	$section = $con->getData("SELECT * FROM section WHERE sect_id = ".$s['stud_sect_id']);
+	$students[$key]['stud_sect_id'] = $section[0];
+	
+	$year = $con->getData("SELECT * FROM year WHERE year_id = ".$s['stud_year_id']);
+	$students[$key]['stud_year_id'] = $year[0];
+	
+	$course = $con->getData("SELECT * FROM course WHERE cour_id = ".$s['f_cour_id']);
+	$students[$key]['f_cour_id'] = $course[0];
+	
+	$logs = $con->getData("SELECT *, DATE_FORMAT(logb_login, '%l:%i') time_in_out FROM logged_book WHERE date(logb_login) = '$newDate' AND stud_id = ".$s['stud_id']);
+	if(empty($logs)){
+		$logs = array(
+			0=> array("time_in_out"=>"No time in/out"),
+			1=> array("time_in_out"=>"No time in/out"),
+			2=> array("time_in_out"=>"No time in/out"),
+			3=>array("time_in_out"=>"No time in/out")
+		);
+	}
+	$students[$key]['logs'] = $logs;
+			
+	
+};
+
+header("Content-Type: application/json");
+echo json_encode($students);
+
+?>
